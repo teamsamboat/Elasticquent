8a46187f05dee11b3f46f5103694e4e95795566b
teamsamboat <35533635+teamsamboat@users.noreply.github.com>
2018-01-17 16:19:10 +0100

    loadRelationsAttributesRecursive()

--- src/ElasticquentTrait.php
+++ src/ElasticquentTrait.php
@@ -710,8 +710,7 @@ trait ElasticquentTrait
             if (method_exists($model, $key)) {
                 $reflection_method = new ReflectionMethod($model, $key);
 
-                // Check if method class has or inherits Illuminate\Database\Eloquent\Model
-                if(!static::isClassInClass("Illuminate\Database\Eloquent\Model", $reflection_method->class)) {
+                if ($reflection_method->class != "Illuminate\Database\Eloquent\Model") {
                     $relation = $model->$key();
 
                     if ($relation instanceof Relation) {
@@ -779,36 +778,4 @@ trait ElasticquentTrait
         }
         return true;
     }
-
-    /**
-     * Check the hierarchy of the given class (including the given class itself)
-     * to find out if the class is part of the other class.
-     *
-     * @param string $classNeedle
-     * @param string $classHaystack
-     * @return bool
-     */
-    private static function isClassInClass($classNeedle, $classHaystack)
-    {
-        // Check for the same
-        if($classNeedle == $classHaystack) {
-            return true;
-        }
-
-        // Check for parent
-        $classHaystackReflected = new \ReflectionClass($classHaystack);
-        while ($parent = $classHaystackReflected->getParentClass()) {
-            /**
-             * @var \ReflectionClass $parent
-             */
-            if($parent->getName() == $classNeedle) {
-                return true;
-            }
-            $classHaystackReflected = $parent;
-        }
-
-        return false;
-
-    }
-
 }
